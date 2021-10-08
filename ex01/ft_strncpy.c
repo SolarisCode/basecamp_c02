9b11c47633c228cb10d1a61e004b51e62e160886
melkholy <melkholy@student.42wolfsburg.de>
2021-10-08 19:32:16 +0200

    Ex01 second commit

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,13 +6,13 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 13:08:39 by coder             #+#    #+#             */
-/*   Updated: 2021/10/07 21:33:05 by coder            ###   ########.fr       */
+/*   Updated: 2021/10/08 19:30:14 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	int	count;
+	unsigned int	count;
 
 	count = 0;
 	while (count < n && src[count] != '\0')
